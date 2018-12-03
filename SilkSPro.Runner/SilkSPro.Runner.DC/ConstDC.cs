64866fc2adc111c4e487269f742df53e5d8e362d
tanitsak leenavuttiwat <tanitsak.lee@silkspan.local>
2018-12-03 18:00:04 +0700

    new

---
+++ SilkSPro.Runner/SilkSPro.Runner.DC/ConstDC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+
+namespace SilkSPro.Runner.DC
+{
+    public static class ConstDC
+    {
+        public static string ADO_CONNECTIONSTRING()
+        {
+            var str = ConfigurationManager.ConnectionStrings["dbServ02"].ConnectionString;
+            return str;
+        }
+    }
+}
